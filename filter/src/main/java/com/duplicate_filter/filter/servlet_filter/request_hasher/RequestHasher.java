303f058a9202d276f059213c1e6e0eb9367565de
Doh <95843713+DohSeungJae@users.noreply.github.com>
2025-10-25 09:38:55 +0000

    [INIT] RequestHasher.java - 요청 정보와 암호화 키를 조합하고 해쉬로 변환하는 컴포넌트

---
+++ filter/src/main/java/com/duplicate_filter/filter/servlet_filter/request_hasher/RequestHasher.java
@@ -0,0 +1,30 @@
+package com.duplicate_filter.filter.servlet_filter.request_hasher;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+import java.util.Base64.Encoder;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RequestHasher {
+
+    @Value("${encryption.key}")
+    private String encryptionKey;
+
+    public String hashify(String content, String ip, String url) throws NoSuchAlgorithmException{
+        String combined=content+"-"+ip+"-"+url+"-"+encryptionKey;
+
+        MessageDigest digest=MessageDigest.getInstance("SHA256");
+        
+        byte[] combinedBytes=combined.getBytes();
+        byte[] encodedHash=digest.digest(combinedBytes);
+        
+        Encoder encoder=Base64.getEncoder();
+        String hashed=encoder.encodeToString(encodedHash);
+
+        return  hashed;
+    } 
+}
