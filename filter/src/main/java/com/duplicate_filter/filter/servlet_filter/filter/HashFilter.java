f76a812132333421b22d59608c8d3495380f7b89
Doh <95843713+DohSeungJae@users.noreply.github.com>
2025-10-26 02:48:59 +0000

    [ADD, ALTER] HashFilter.java - denyTheRequest() 메서드 구현, getContentFromRequest() 메서드에 예외 처리 코드 추가, getRequestDetails() 메서드의 반환 타입을 Map에서 RequestDetails로 변경

--- filter/src/main/java/com/duplicate_filter/filter/servlet_filter/filter/HashFilter.java
+++ filter/src/main/java/com/duplicate_filter/filter/servlet_filter/filter/HashFilter.java
@@ -3,12 +3,13 @@ package com.duplicate_filter.filter.servlet_filter.filter;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
-import java.util.HashMap;
 import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+import com.duplicate_filter.filter.servlet_filter.DTO.HashedRequestDetails;
+import com.duplicate_filter.filter.servlet_filter.DTO.RequestDetails;
 import com.duplicate_filter.filter.servlet_filter.cached_body.CachedBodyHttpServletRequest;
 import com.duplicate_filter.filter.servlet_filter.request_hasher.RequestHasher;
 import com.duplicate_filter.filter.servlet_filter.request_store.RequestStore;
@@ -25,7 +26,7 @@ import lombok.extern.slf4j.Slf4j;
 public class HashFilter extends OncePerRequestFilter implements HttpFilter {
 
     @Autowired
-    @Qualifier("hashMapRequestStore") //bean name을 파라미터로 넣는데, spring bean의 작명 컨벤션은 카멜 케이스임
+    @Qualifier("hashMapRequestStore") 
     private RequestStore store;
 
     @Autowired
@@ -38,14 +39,13 @@ public class HashFilter extends OncePerRequestFilter implements HttpFilter {
         //request에서 직접 body를 가져오면 Controller에서는 body를 읽을 수 없기 때문에 body를 읽기 전에 미리 cache하도록 함
         CachedBodyHttpServletRequest cachedRequest=new CachedBodyHttpServletRequest(request);
 
-        HashMap<String,String> requestDetails=this.getRequestDetails(cachedRequest);
-        String hashedRequest=requestDetails.get("hashedRequest");
+        HashedRequestDetails requestDetails=(HashedRequestDetails)this.getRequestDetails(cachedRequest);
+        String hashedRequest=requestDetails.getHashedRequest();
         
         Boolean duplicate=store.isDuplicateOrElseStore(hashedRequest);
         if(duplicate){
-            response.setStatus(429); //Too Many Requests
-            response.setContentType("application/json");
-            response.getWriter().write("{\"error\": \"Duplicate request detected. Please try again later.\"}"); //Object Mapper 사용하기
+            log.warn("Duplicate Request Occured");
+            denyTheRequest(response);
             return ;
         }else{
             filterChain.doFilter(cachedRequest, response);
@@ -53,12 +53,10 @@ public class HashFilter extends OncePerRequestFilter implements HttpFilter {
     }
 
     @Override
-    public HashMap<String, String> getRequestDetails(HttpServletRequest request) throws IOException{
+    public RequestDetails getRequestDetails(HttpServletRequest request) throws IOException{
         if (!(request instanceof CachedBodyHttpServletRequest)) {
             throw new IllegalArgumentException("Expected CachedBodyHttpServletRequest but received " + request.getClass().getSimpleName());
         }
-
-        HashMap<String, String> hashMap=new HashMap<>();
 
         String content=getContentFromRequest(request);
         String ipAddress=request.getRemoteAddr(); //body에 접근하지 않기 때문에 body는 소모되지 않음
@@ -70,8 +68,18 @@ public class HashFilter extends OncePerRequestFilter implements HttpFilter {
             throw new RuntimeException("Algorithm for hashing not correct"); //RuntimeError로 처리하면 안되는데... Custom Exception을 만들어야해..
         } 
 
-        hashMap.put("hashedRequest",hashedRequest);
-        return hashMap;
+        HashedRequestDetails requestDetails=HashedRequestDetails.builder()
+            .hashedRequest(hashedRequest)
+            .build();
+        
+        return requestDetails;
+    }
+
+    @Override
+    public void denyTheRequest(HttpServletResponse response) throws IOException{
+        response.setStatus(429); //Too Many Requests
+        response.setContentType("application/json");
+        response.getWriter().write("{\"error\": \"Duplicate request detected. Please try again later.\"}");
     }
 
     public String getContentFromRequest(HttpServletRequest cachedRequest) throws IOException{ //사실상 CachedBodyHttpServletRequest을 받음
@@ -80,10 +88,16 @@ public class HashFilter extends OncePerRequestFilter implements HttpFilter {
         try(BufferedReader reader=new BufferedReader(cachedRequest.getReader())){
             body=reader.lines().collect(Collectors.joining("\n"));
         }
+
         JsonNode jsonNode=mapper.readTree(body);
-        String value=jsonNode.get("string").asText();
-        
-        return value;
+        JsonNode stringNode=jsonNode.get("string");
+
+        if(stringNode==null || stringNode.isNull()){
+            throw new IllegalArgumentException("Missing required filed: 'string'");
+        }
+
+        String content=jsonNode.get("string").asText();
+        return content;
     }
 
 
