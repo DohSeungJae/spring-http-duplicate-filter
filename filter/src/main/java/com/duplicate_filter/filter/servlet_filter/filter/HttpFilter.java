af44dde5591bd32b7b8d8ada8dfb0bccaf297e2f
Doh <95843713+DohSeungJae@users.noreply.github.com>
2025-10-26 02:46:17 +0000

    [ALTER, UPDATE] HttpFilter.java  - getRequestDetails() 메서드의 반환 타입을 RequestDetails로 변경, denyTheRequest() 추상 메서드 추가

--- filter/src/main/java/com/duplicate_filter/filter/servlet_filter/filter/HttpFilter.java
+++ filter/src/main/java/com/duplicate_filter/filter/servlet_filter/filter/HttpFilter.java
@@ -3,10 +3,15 @@ package com.duplicate_filter.filter.servlet_filter.filter;
 import java.io.IOException;
 import java.util.HashMap;
 
+import com.duplicate_filter.filter.servlet_filter.DTO.RequestDetails;
+
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 public interface HttpFilter{
     
-    public HashMap<String, String> getRequestDetails(HttpServletRequest request) throws IOException;
+    public RequestDetails getRequestDetails(HttpServletRequest request) throws IOException;
+
+    public void denyTheRequest(HttpServletResponse response) throws IOException;
 
 }
