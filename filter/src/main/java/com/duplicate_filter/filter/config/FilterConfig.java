80511ba6b3cd0324588c4c7386fd37e6a60f5d95
Doh <95843713+DohSeungJae@users.noreply.github.com>
2025-11-21 12:02:23 +0000

    [ADD] FilterConfig.java - 필터 등록 빈에 Profile 어노테이션을 추가하여 Filter를 활성화/비활성화 할 수 있도록 함

--- filter/src/main/java/com/duplicate_filter/filter/config/FilterConfig.java
+++ filter/src/main/java/com/duplicate_filter/filter/config/FilterConfig.java
@@ -3,7 +3,9 @@ package com.duplicate_filter.filter.config;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import com.duplicate_filter.filter.servlet_filter.filter.HashFilter;
+import com.duplicate_filter.filter.servlet_filter.filter.IdempotencyKeyFilter;
 
 @Configuration
 public class FilterConfig {
@@ -12,8 +14,9 @@ public class FilterConfig {
     //Spring Container는 IoC를 통해 의존성을 효율적으로 관리할 수 있게 만들어줌
     //IoC는 객체의 생성과 의존성 주입을 개발자가 직접 하지 않고 프레임 워크(Spring Container)가 
     //대신 수행함으로써 제어 흐름이 역전되는 구조를 의미함.
-     
+    
     @Bean
+    @Profile("hash")
     public FilterRegistrationBean<HashFilter> loggingHashFilter(HashFilter hashFilter){ //Bean을 주입받도록 설정 
         FilterRegistrationBean<HashFilter> registrationBean=new FilterRegistrationBean<>();
 
@@ -22,7 +25,19 @@ public class FilterConfig {
         registrationBean.addUrlPatterns("/*");
         //모든 요청에 대해 필터링을 수행하도록 설정
         registrationBean.setOrder(0); 
-        //최우선 순위로 필터링
+        //우선 순위 설정 (0이 최우선순위)
+
+        return registrationBean;
+    }
+
+    @Bean
+    @Profile("idem")
+    public FilterRegistrationBean<IdempotencyKeyFilter> loggingIdempotencyKeyFilter(IdempotencyKeyFilter idempotencyKeyFilter){
+        FilterRegistrationBean<IdempotencyKeyFilter> registrationBean=new FilterRegistrationBean<>();
+
+        registrationBean.setFilter(idempotencyKeyFilter);
+        registrationBean.addUrlPatterns("/*");
+        registrationBean.setOrder(0);
 
         return registrationBean;
     }
