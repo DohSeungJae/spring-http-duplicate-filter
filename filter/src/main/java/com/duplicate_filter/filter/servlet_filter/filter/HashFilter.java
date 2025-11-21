d83c71251a636273da972cc8911fe5c5afe1f2eb
Doh <95843713+DohSeungJae@users.noreply.github.com>
2025-11-21 14:05:21 +0000

    [EDIT, ADD] HashFilter.java - 버퍼의 불필요한 중복 래핑 개선, 메서드 체인 모두 분리, 요청 바디가 없는 경우 예외처리 코드 추가

--- filter/src/main/java/com/duplicate_filter/filter/servlet_filter/filter/HashFilter.java
+++ filter/src/main/java/com/duplicate_filter/filter/servlet_filter/filter/HashFilter.java
@@ -2,14 +2,18 @@ package com.duplicate_filter.filter.servlet_filter.filter;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.security.NoSuchAlgorithmException;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 import com.duplicate_filter.filter.servlet_filter.cached_body.CachedBodyHttpServletRequest;
+import com.duplicate_filter.filter.servlet_filter.exception.MissingRequestBodyException;
 import com.duplicate_filter.filter.servlet_filter.request_hasher.RequestHasher;
 import com.duplicate_filter.filter.servlet_filter.request_store.RequestStore;
 import com.fasterxml.jackson.databind.JsonNode;
@@ -38,11 +42,17 @@ public class HashFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException{
         //request에서 직접 body를 가져오면 Controller에서는 body를 읽을 수 없기 때문에 body를 읽기 전에 미리 cache하도록 함
         CachedBodyHttpServletRequest cachedRequest=new CachedBodyHttpServletRequest(request);
+        String hashedRequest;
+        try{
+            hashedRequest=getHashedRequest(cachedRequest);
+        }catch(MissingRequestBodyException e){
+            denyTheRequest(response, 400, e.getMessage());
+            return ;
+        }
 
-        String hashedRequest=getHashedRequest(cachedRequest);
-        Boolean duplicate=store.isDuplicateOrElseStore(hashedRequest);
+        boolean duplicate=store.isDuplicateOrElseStore(hashedRequest);
         if(duplicate){
-            log.warn("Duplicate Request Occured");
+            log.warn("Duplicate request occured");
             denyTheRequest(response);
             return ;
         }else{
@@ -53,10 +63,11 @@ public class HashFilter extends OncePerRequestFilter {
     public String getHashedRequest(CachedBodyHttpServletRequest request) throws IOException{
         String content=getContentFromRequest(request);
         String ipAddress=request.getRemoteAddr(); //body에 접근하지 않기 때문에 body는 소모되지 않음
-        String requestURL=request.getRequestURL().toString(); //body에 접근하지 않기 때문에 body는 소모되지 않음 //.getRequestURL()은 StringBuffer를 반환하기 때문에 .toString()이 필요함
+        StringBuffer requestURL=request.getRequestURL(); //body에 접근하지 않기 때문에 body는 소모되지 않음
+        String requestURLString=requestURL.toString(); 
         String hashedRequest;
         try{
-            hashedRequest=hasher.hashify(content, ipAddress, requestURL);
+            hashedRequest=hasher.hashify(content,ipAddress,requestURLString);
         }catch(NoSuchAlgorithmException e){
             throw new IllegalArgumentException("Algorithm for hashing not correct");
         }
@@ -67,24 +78,35 @@ public class HashFilter extends OncePerRequestFilter {
     public String getContentFromRequest(HttpServletRequest cachedRequest) throws IOException{ //사실상 CachedBodyHttpServletRequest을 받음
         String body;
         //try-with-resource, reader 사용이 끝나면 JVM이 reader.close()를 자동으로 호출 <- 리소스 누수 방지
-        try(BufferedReader reader=new BufferedReader(cachedRequest.getReader())){
-            body=reader.lines().collect(Collectors.joining("\n"));
+        try(BufferedReader reader=cachedRequest.getReader()){
+            Stream<String> lines=reader.lines();
+            Collector<CharSequence, ?, String> coll=Collectors.joining("\n");
+            body=lines.collect(coll);
         }
 
         JsonNode jsonNode=mapper.readTree(body);
         JsonNode stringNode=jsonNode.get("string");
-        if(stringNode==null || stringNode.isNull()){
-            throw new IllegalArgumentException("Missing required filed: 'string'");
+        String trimedBody=body.trim();
+        if(trimedBody.isEmpty() || stringNode==null){ 
+            throw new MissingRequestBodyException("Request body is missing");
         }
-        String content=jsonNode.get("string").asText();
+
+        String content=stringNode.asText();
         return content;
     }
 
-    
     public void denyTheRequest(HttpServletResponse response) throws IOException{
         response.setStatus(429); //Too Many Requests
         response.setContentType("application/json");
-        response.getWriter().write("{\"error\": \"Duplicate request detected. Please try again later.\"}");
+        PrintWriter writer=response.getWriter();
+        writer.write("{\"error\": \"Duplicate request detected. Please try again later.\"}");
+    }
+
+    public void denyTheRequest(HttpServletResponse response, int status, String message) throws IOException{
+        response.setStatus(status); 
+        response.setContentType("application/json");
+        PrintWriter writer=response.getWriter();
+        writer.write("{\"error\": \"" + message + "\"}");
     }
 
     
